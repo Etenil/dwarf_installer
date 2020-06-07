c05076fd7a84aed6320d5137301f0f045da6257b
Guillaume Pasquet <dev@etenil.net>
2020-06-07 17:26:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DWARF INSTALLER
+Installer for dwarf fortress on Linux.
+
+## How to use
+Clone this repository, or download the `df_install.sh` script,
+then run the script with bash.
+
+## End result
+Dwarf fortress will be installed in `$HOME/.local/opt` and a
+desktop file is generated so it can be started from your desktop
+environment easily.
+
---
+++ df_install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DF_URL="https://www.bay12games.com/dwarves/df_47_04_linux.tar.bz2"
+DF_TAR=/tmp/df.tar.bz2
+DF_HOME="$HOME/.local/opt/dwarf-fortress"
+ICON_URL="https://mthec.files.wordpress.com/2009/08/dwarf.png"
+ICON_TMP=/tmp/df.png
+ICON_FINAL="$DF_HOME/icon.png"
+DESKTOP_FILE="$HOME/.local/share/applications/dwarf-fortress.desktop"
+
+download() {
+    # Abstract CURL and WGET so we can work on any distro.
+    echo "Downloading $1..."
+    if hash curl 2> /dev/null; then
+        curl -L "$1" > $2
+    else
+        wget -O $2 "$1"
+    fi
+}
+
+download $DF_URL $DF_TAR
+download $ICON_URL $ICON_TMP
+
+mkdir -p $DF_HOME
+
+tar -xjf $DF_TAR -C $DF_HOME --strip-components=1
+mv $ICON_TMP $ICON_FINAL
+
+# HACK: delete shipped libstdc++ that is incompatible with system
+# in modern linux distros
+rm $DF_HOME/libs/libstdc++.so.6
+
+cat <<EOF > $DESKTOP_FILE
+[Desktop Entry]
+Name=Dwarf Fortress
+Comment=dwarf fortress
+Exec=$DF_HOME/df
+Icon=$ICON_FINAL
+Terminal=false
+Type=Application
+Categories=Game;
+StartupWMClass=Dwarf_Fortress
+EOF
+
+echo "Done"
+echo "You now need to ensure you have the necessary dependencies"\
+echo "For fedora, run:"
+echo "       sudo dnf install SDL SDL_ttf SDL_sound SDL_mixer SDL_image mesa-libGLU openal-soft"
+
